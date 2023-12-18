2d40aacca353b124b045194599b200a3daa1a547
MiraCurAll Consultancy Services <info.miracurallconsultancy@gmail.com>
2023-12-18 15:38:45 +0530

    Delete actions directory

--- actions/index.js
+++
@@ -1,6 +0,0 @@
-// actions/index.js
-
-export { default as Line } from './line.js';
-export { default as Arc } from './arc.js';
-export { default as Text } from './text.js';
-export { default as Eraser } from './eraser.js';
