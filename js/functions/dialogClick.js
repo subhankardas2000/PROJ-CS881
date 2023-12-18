f8e1b4e2e321651ace1ffbfed20d443b60f8f449
MiraCurAll Consultancy Services <info.miracurallconsultancy@gmail.com>
2023-12-18 15:37:20 +0530

    Add files via upload

---
+++ js/functions/dialogClick.js
@@ -0,0 +1,16 @@
+// functions/dialogClick.js
+
+window.addEventListener("load", function() {
+    var dialogs = document.getElementsByClassName("dialog");
+    Array.prototype.forEach.call(dialogs, dialogHandler);
+  });
+  
+  function dialogHandler(dialog) {
+    dialog.addEventListener("click", dialogClick);
+  }
+  
+  export default function dialogClick(e) {
+    if (e.target.className === "dialog") {
+      e.target.style.display = "none";
+    }
+  }
---
+++ js/functions/selectChange.js
@@ -0,0 +1,60 @@
+// functions/selectChange.js
+
+import drawing from './../drawing.js';
+import options from './../options.js';
+
+window.addEventListener("load", function() {
+  var selects = document.getElementById('options').getElementsByTagName("select");
+  Array.prototype.forEach.call(selects, selectHandler);
+
+  // this maybe should go in drawing, but it is a class so not sure how
+  document.getElementById("name").addEventListener("change", e => {
+    var desired = e.target.value.replace(/[^a-z0-9_\-\s.'()]/gi, '');
+    if (desired === "") {
+      desired = "Drawing";
+    }
+    drawing.setName(desired);
+    document.getElementById("name").value = desired;
+  });
+});
+
+function selectHandler(select) {
+  select.addEventListener("change", selectChange);
+}
+
+export default function selectChange(e) {
+  var youChanged = {
+    "colortheme": function() {
+      options.change('colortheme', e.target.value);
+      drawing.refresh(true);
+    },
+    "grid": function() {
+      options.change('grid', 96 / e.target.value);
+      drawing.refresh(true);
+    },
+    "divisions": function() {
+      options.change('divisions', e.target.value);
+      drawing.refresh(true);
+    },
+    "tooltip": function() {
+      options.change('tooltip', e.target.value);
+    },
+    "fractions": function() {
+      options.change('fractions', e.target.value);
+    },
+    "gridsnap": function() {
+      options.change('gridsnap', e.target.value);
+    },
+    "lengthsnap": function() {
+      options.change('lengthsnap', e.target.value);
+    },
+    "anglesnap": function() {
+      options.change('anglesnap', e.target.value);
+    },
+    "endsnap": function() {
+      options.change('endsnap', e.target.value);
+    }
+  };
+
+  youChanged[e.target.id]();
+}
