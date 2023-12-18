888906047dded7cc6278107575e15e8c767f3be5
MiraCurAll Consultancy Services <info.miracurallconsultancy@gmail.com>
2023-12-18 15:37:07 +0530

    Create buttonClick.js

---
+++ js/functions/buttonClick.js
@@ -0,0 +1,54 @@
+// functions/buttonClick.js
+
+import tool from './../tool.js';
+import { Line, Arc, Text, Eraser } from '../actions/index.js';
+import drawing from './../drawing.js';
+
+window.addEventListener("load", function() {
+  var buttons = document.getElementsByTagName("button");
+  Array.prototype.forEach.call(buttons, buttonHandler);
+});
+
+function buttonHandler(button) {
+  button.addEventListener("click", buttonClick);
+}
+
+export default function buttonClick(e) {
+  var youClicked = {
+    "line-button": function() {
+      tool.change(new Line());
+    },
+    "arc-button": function() {
+      tool.change(new Arc());
+    },
+    "text-button": function() {
+      tool.change(new Text());
+    },
+    "eraser-button": function() {
+      tool.change(new Eraser());
+    },
+    "undo-button": function() {
+      drawing.undo();
+    },
+    "options-button": function() {
+      document.getElementById("options").style.display = "block";
+    },
+    "options-close": function() {
+      document.getElementById("options").style.display = "none";
+    },
+    "save": function() {
+      drawing.save();
+    },
+    "export": function() {
+      drawing.xport();
+    },
+    "import": function() {
+      drawing.import();
+    },
+    "clear": function() {
+      drawing.clear();
+    }
+  };
+
+  youClicked[e.currentTarget.id]();
+}
